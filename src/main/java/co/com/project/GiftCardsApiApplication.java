af8a9df367957075d1b9d95a9cb8533c0f922039
Andres Avendaño <andresavendano@MacBook-Pro-de-Andres.local>
2025-02-21 14:55:30 -0500

    first commit

---
+++ src/main/java/co/com/project/GiftCardsApiApplication.java
@@ -0,0 +1,13 @@
+package co.com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftCardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftCardsApiApplication.class, args);
+	}
+
+}
